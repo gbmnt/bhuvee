bc569f064d2bec95971219c9994903f7fc31e176
Unic-1 <satyam123.sr@gmail.com>
2018-09-10 23:30:12 +0530

    Title change

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Learning GSAP</title>
+  <title>Bhuvee</title>
   <link rel="shortcut icon" type="image/x-icon" href="img/logo.png" />
   <link rel="stylesheet" href="master.css">
   <link href="Main.css" rel="stylesheet" />
