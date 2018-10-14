a6d0ad7f3b9aa7e8f1749f6bf88d4a80b8920e84
Unic-1 <satyam123.sr@gmail.com>
2018-10-14 12:21:32 +0530

    Tab Icon added to all the pages SendGrid api key updated

--- mail.php
+++ mail.php
@@ -7,34 +7,23 @@ require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // replacing <PATH TO> with the path to the sendgrid-php.php file,
 // which is included in the download:
 // https://github.com/sendgrid/sendgrid-php/releases
-// $first_name = $_POST['first_name'];
-// $last_name = $_POST['last_name'];
-// $email_address = $_POST['email'];
-// $address = $_POST['address'];
-// $state = $_POST['state'];
-// $city = $_POST['city'];
-// $pincode = $_POST['pincode'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email_address = $_POST['email'];
+$address = $_POST['address'];
+$state = $_POST['state'];
+$city = $_POST['city'];
+$pincode = $_POST['pincode'];
 
-// echo $first_name;
-// echo $last_name;
-// echo $email_address;
-// echo $address;
-// echo $state;
-// echo $city;
-// echo $pincode;
+echo $email_address;
 
 $email = new \SendGrid\Mail\Mail(); 
-// $email->setSmtpapiTos(["satyam123.sr@gmail.com"]);
-// $email->setFrom($email_address, "Bhuvee Client");
-$email->setFrom("satyam123.sr@gmail.com", "Bhuvee Client");
+$email->setFrom($email_address, "Bhuvee Client");
 $email->setSubject("Enquiry");
-// $email->addTo("satyam123.sr@gmail.com", $first_name);
-$email->addTo("satyam123.sr@gmail.com", "First");
-$email->addContent("text/html", "this is a test mail");
-// $email->addContent("text/html", "Item: ".$item."\n\r".$message."\n\rPhone No: ".$phone_no);
-// $email->addContent("text/html", "Name: ".$first_name." ".$last_name."<br>Address: ".$address."<br>State: ".$state."<br>City: ".$city."<br>Pincode: ".$pincode);
+$email->addTo("veena.bhuvee@gmail.com", "First");
+$email->addContent("text/html", "Name: ".$first_name." ".$last_name."<br>Address: ".$address."<br>State: ".$state."<br>City: ".$city."<br>Pincode: ".$pincode);
 
-$sendgrid = new \SendGrid('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');
+$sendgrid = new \SendGrid('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');
 try {
     $response = $sendgrid->send($email);
     print $response->statusCode() . "\n";
